ffcf934e277e6603ab91b23dd8e8332cd5eaf67b
unknown <helioengracialipinze@gmail.com>
2021-09-19 02:53:05 -0700

    Projecto galeria de imagens

---
+++ README.md
@@ -0,0 +1,3 @@
+## Projecto Galeria de imagens com HTML, CSS e JavasCript para Youtube
+
+[Link do canal CssDescomplicado:](https://www.youtube.com/channel/UCT7ziRh_Mhc1cFsN_-rdn4w) 
---
+++ app.js
@@ -0,0 +1,46 @@
+const btns = document.querySelectorAll(".btn");
+
+btns.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    makeActiveBtn(e);
+
+    if (e.target.classList.contains("btn__all")) {
+      filterImage("all");
+    }
+    if (e.target.classList.contains("btn__car")) {
+      filterImage("car");
+    }
+    if (e.target.classList.contains("btn__house")) {
+      filterImage("house");
+    }
+    if (e.target.classList.contains("btn__animal")) {
+      filterImage("animal");
+    }
+  });
+});
+
+const makeActiveBtn = (event) => {
+  btns.forEach((btn) => {
+    btn.classList.remove("active");
+  });
+
+  event.target.classList.add("active");
+};
+
+const filterImage = (category) => {
+  const images = Array.from(document.querySelectorAll(".filter__imgs img"));
+
+  images.forEach((img) => {
+    img.classList.add("hide");
+  });
+
+  const filterdImages = images.filter(
+    (img) =>
+      img.getAttribute("data-image") === category ||
+      img.getAttribute("data-all") === category
+  );
+
+  filterdImages.forEach((img) => {
+    img.classList.remove("hide");
+  });
+};
